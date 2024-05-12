38782a7ec0dd36f1bc6bb139e4aa954534eeacc5
Your Name <you@example.com>
2024-05-12 18:15:50 +0200

    Ensivideo

--- C/src/ensivideo.c
+++ C/src/ensivideo.c
@@ -2,13 +2,15 @@
 #include <assert.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <threads.h>
 
 #include "oggstream.h"
 #include "stream_common.h"
 
 int main(int argc, char *argv[]) {
-  int res;
 
+  int res;
+  thrd_t theora_pid, vorbis_pid;
   if (argc != 2) {
     fprintf(stderr, "Usage: %s FILE", argv[0]);
     exit(EXIT_FAILURE);
@@ -24,8 +26,12 @@ int main(int argc, char *argv[]) {
   // start the two stream readers (theoraStreamReader and vorbisStreamReader)
   // each in a thread
   
+  thrd_create(&theora_pid, theoraStreamReader, argv[0]);
+  thrd_create(&vorbis_pid, vorbisStreamReader, argv[0]);
+
   // wait for vorbis thread
 
+  thrd_join(vorbis_pid, NULL);
   // 1 seconde of sound in advance, thus wait 1 seconde
   // before leaving
   sleep(1);
